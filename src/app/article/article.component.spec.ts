97a468ca9ec9b4af8d7b740c612b44485bd408a1
thommibens <thomas.benetton.to@gmail.com>
2022-04-15 16:47:05 +0200

    Fix: test should import required module

--- src/app/article/article.component.spec.ts
+++ src/app/article/article.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
+import { RouterTestingModule } from '@angular/router/testing';
 import { ArticleComponent } from './article.component';
 
 describe('ArticleComponent', () => {
@@ -8,6 +9,7 @@ describe('ArticleComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [ ArticleComponent ]
     })
     .compileComponents();
